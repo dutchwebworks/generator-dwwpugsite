200f69039d9d2577bab6a7f29135725ce4ca148f
Dennis Burger <dennis@dutchwebworks.nl>
2016-12-29 20:10:43 +0100

    Working Gulp compile of Sass and Pug templates.

--- app/templates/Gulpfile.js
+++ app/templates/Gulpfile.js
@@ -7,11 +7,14 @@
 **********************************************************************/
 
 var gulp = require('gulp'),
-	browserSync = require('browser-sync'),
+	browserSync = require('browser-sync').create(),
 	$ = require('gulp-load-plugins')(),
+	pug = require('gulp-pug'),
+	gulp_watch_pug = require('gulp-watch-pug')
+	pkg = require('./package.json'),
 	reload = browserSync.reload,
 	src = './src'
-	dist = './dist'
+	dist = './dist',
 	config = {
 		htmlPath: dist,
 		scssPath: src + '/scss',
@@ -20,10 +23,20 @@ var gulp = require('gulp'),
 		jsPathDest: dist + '/js',
 		imgPathSrc: src + '/img',
 		imgPathDest: dist + '/img'
+	},
+	meta = {
+		banner: ['/*!',
+			' * Name: <%= pkg.name %>',
+			' * Author: <%= pkg.author %>',
+			' * Version: <%= pkg.version %>',
+			' * Date: ' + formatDate(),
+			' */',
+			'',
+		''].join('\n')
 	};
 
 /**********************************************************************
-2. Banner info prefixed to minified files (see `package.json`)
+2. Custom functions
 **********************************************************************/
 
 // Generate pretty date for banner header in minified files
@@ -37,20 +50,6 @@ function formatDate() {
 	return (today.getFullYear() + '-' + month) + '-' + day;
 }
 
-// Meta-data and other vars
-var pkg = require('./package.json'),
-	dist = './httpdocs',
-	meta = {
-		banner: ['/*!',
-	  		' * Name: <%= pkg.name %>',
-	  		' * Author: <%= pkg.author %>',
-	  		' * Version: <%= pkg.version %>',
-	  		' * Date: ' + formatDate(),
-	  		' */',
-	  		'',
-	  	''].join('\n')
-	};
-
 /**********************************************************************
 3. Configure Gulp tasks
 **********************************************************************/
@@ -58,105 +57,33 @@ var pkg = require('./package.json'),
 /* Sass compile with sourcemap
 -------------------------------------------------------------------- */
 
-gulp.task('sass:dev', function(){
-	return gulp.src(dist + '/sass/**/*.scss')
-		.pipe($.newer(dist + '/css'))
-		.pipe($.sourcemaps.init())
-		.pipe($.sass({
-			style: 'extended',
-			sourceComments: 'map',
-			sourcemap: true,
-			errLogToConsole: true
-		}))
-		.pipe($.sourcemaps.write('./'))
-		.pipe(gulp.dest(dist + '/css'));
-});
-
-gulp.task('sass:dist', function(){
-	return gulp.src(dist + '/sass/**/*.scss')
-		.pipe($.newer(dist + '/css'))
+gulp.task('sass', function(){
+	return gulp.src(config.scssPath + '/**/*.scss')
+		.pipe($.newer(config.cssPath + '/css'))
 		.pipe($.sass({
 			style: 'extended',
 			sourcemap: false,
 			errLogToConsole: true
 		}))
-		.pipe(gulp.dest(dist + '/css'));
+		.pipe(gulp.dest(config.cssPath));
 });
 
-/* Concatenate and minify Css files
+/* Compile Pug templates
 -------------------------------------------------------------------- */
 
-gulp.task('cssmin', function(){
-	return gulp.src([
-			dist + '/css/style.css'
-		])
-		.pipe($.concat('style.min.css'))
-		.pipe($.minifyCss({
-			keepBreaks: false,
-			removeEmpty: true
+gulp.task('pug', function buildHTML() {
+	return gulp.src(src + '/*.pug')
+		.pipe(pug({
+			pretty: true
 		}))
-		.pipe($.header(meta.banner, {
-			pkg : pkg,
-		 } ))
-		.pipe(gulp.dest(dist + '/css'));
-});
-
-/* Optimize JPG, PNG and GIF's
--------------------------------------------------------------------- */
-
-gulp.task('imagemin', function(){
-	return gulp.src(dist + '/img/**/*.{png,jpg,gif}')
-		.pipe($.imagemin({
-			optimazationLevel: 5,
-			progressive: false,
-			interlaced: true
-		}))
-		.pipe(gulp.dest(dist + '/img'));
-});
-
-/* Optimize SVG's
--------------------------------------------------------------------- */
-
-gulp.task('svgmin', function(){
-	return gulp.src(dist + '/img/**/*.svg')
-		.pipe($.svgmin())
-		.pipe(gulp.dest(dist + '/img'));
-});
-
-/* Jshint lint Javascript files
--------------------------------------------------------------------- */
-
-gulp.task('jshint', function(){
-	return gulp.src(dist + '/js/*.js')
-		.pipe($.jshint())
-		.pipe($.jshint.reporter('default'));
-});
-
-/* Concatenate and uglify Javascript files
--------------------------------------------------------------------- */
-
-gulp.task('uglify', function(){
-	return gulp.src([
-			dist + '/js/libs/jquery-2.1.0.js',
-			dist + '/js/libs/modernizr-2.7.1.js',
-			dist + '/js/common.js'
-		])
-		.pipe($.concat('common.js', {
-			newLine: ';'
-		}))
-		.pipe($.uglify())
-		.pipe($.header(meta.banner, {
-			pkg : pkg,
-		 } ))
-		.pipe(gulp.dest(dist + '/js/min'));
+		.pipe(gulp.dest(dist));
 });
 
 /* Run a proxy server
 -------------------------------------------------------------------- */
 
 gulp.task('browser-sync', function() {
-	browserSync({
-		// proxy: { 'gulp-test.local.cassius.nl' }
+	browserSync.init({
 		server: {
 			baseDir: dist
 		}
@@ -168,31 +95,27 @@ gulp.task('browser-sync', function() {
 
 gulp.task('clean', function(){
 	gulp.src([
-		dist + '/css/**/*.map',
-		dist + '/js/**/*.map',
+		config.cssPath + '/**/*.map',
+		config.jsPathDest + '/**/*.map'
 		],{read: false})
-		.pipe($.clean());
+	.pipe($.clean());
 });
 
 /**********************************************************************
 4. Registered Gulp tasks
 **********************************************************************/
 
-// Server (proxy) and file `watcher` livereload for local development
+gulp.task('build', ['clean'], function(){
+	gulp.start('pug');
+	gulp.start('sass');
+	gulp.start('copy');
+});
+
 gulp.task('serve', ['browser-sync'], function(){
-	gulp.watch(dist + '/*.html', [reload]);
-	gulp.watch(dist + '/css/*.css', [reload]);
-	gulp.watch(dist + '/sass/**/*.scss', ['sass:dev', reload]);
+	gulp.watch(dist + '/*.html', [browserSync.reload]);
+	gulp.watch(config.cssPath + '/*.css', [browserSync.reload]);
+	gulp.watch(src + '/*.pug', ['pug', browserSync.reload]);
+	gulp.watch(config.scssPath + '/**/*.scss', ['sass', browserSync.reload]);
 });
 
-// Aliasses, sub-tasks
-gulp.task('build-sass', ['sass:dist'], function(){
-	gulp.start('cssmin');
-});
-gulp.task('build-js', ['uglify']);
-gulp.task('build-img', ['imagemin', 'svgmin']);
-
-// Run once for deployment, build everything using above aliasses and run a cleanup task
-gulp.task('default', ['build-sass', 'build-js', 'build-img'], function(){
-	gulp.start('clean');
-});
+// gulp.task('default', ['build']);
